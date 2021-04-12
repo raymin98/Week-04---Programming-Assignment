d4c8dfb9974e94ccf516a6a23c51e910228b5839
raymin98 <raymin1998@yahoo.com>
2021-04-11 23:34:45 -0400

    Week 4
    Count 3 more letters

--- Week 04 - Programming Assignment.cpp
+++ Week 04 - Programming Assignment.cpp
@@ -1,15 +1,16 @@
 // Week 04 - Programming Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //#include <stdafx.h>
+
 #include<iostream>
 #include<cstring>
 #include<fstream>
 using namespace std;
-//this function count the vowels
+
 int count_vowels(string line)
 {
-    //variables
+
     int i, vowels = 0;
-    int A = 0, O = 0, I = 0, E = 0, U = 0;
+    int A = 0, O = 0, I = 0, E = 0, U = 0, R = 0, X = 0, Z = 0;
     for (int i = 0; i < line.length(); i++)
     {
         if (line[i] == 'a' || line[i] == 'A')
@@ -31,7 +32,7 @@ int count_vowels(string line)
 
         }
 
-        else if (line[i] == 'I' || line[i] == 'i')
+        else if (line[i] == 'i' || line[i] == 'I')
         {
             I++;
             vowels++;
@@ -42,27 +43,44 @@ int count_vowels(string line)
             U++;
             vowels++;
         }
-
+        else if (line[i] == 'r' || line[i] == 'R')
+        {
+            R++;
+            vowels++;
+        }
+        else if (line[i] == 'x' || line[i] == 'X')
+        {
+            X++;
+            vowels++;
+        }
+        else if (line[i] == 'z' || line[i] == 'Z')
+        {
+            Z++;
+            vowels++;
+        }
     }
     cout << "The number of A's:......................" << A << endl;
     cout << "The number of E's:......................" << E << endl;
     cout << "The number of I's:......................" << I << endl;
     cout << "The number of O's:......................" << O << endl;
     cout << "The number of U's:......................" << U << endl;
+    cout << "The number of R's:......................" << R << endl;
+    cout << "The number of X's:......................" << X << endl;
+    cout << "The number of Z's:......................" << Z << endl;
     return vowels;
 }
-int main(int argc, char* argv[])
+int main(int argc, char* Constitution_of_the_United_States[])
 {
     ifstream infile;
-    infile.open(argv[1]);
-    cout << "Analyzing file data.txt........." << endl;
-    cout << "******************" << endl;
-    cout << "***Welcome to my Letter Count Program***" << endl;
-    cout << "******************" << endl;
+    infile.open(Constitution_of_the_United_States[1]);
+    cout << "Analyzing file data.txt'Constitution_of_the_United_States.txt'....." << endl;
+    cout << "*****************************************************" << endl;
+    cout << "*********Welcome to my Letter Count Program**********" << endl;
+    cout << "*****************************************************" << endl;
     string s;
     int i, vowels;
     //call to function
-    while (getline(infile, s))
+    while (Constitution_of_the_United_States(infile, s))
     {
         vowels = count_vowels(s);
         cout << "The vowels count is:...................." << vowels << endl;
@@ -71,3 +89,4 @@ int main(int argc, char* argv[])
 
     return 0;
 }
+
